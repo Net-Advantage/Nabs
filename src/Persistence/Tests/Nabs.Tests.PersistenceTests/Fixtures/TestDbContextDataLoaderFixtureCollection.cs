aa93cdec7af06bd381d73eca412e339701d61d7c
taxlabDarrel <darrel@taxlab.online>
2022-01-30 16:09:37 +1300

    Another renaming and some cleanup.

--- src/Persistence/Tests/Nabs.Tests.PersistenceTests/Fixtures/TestDbContextDataLoaderFixtureCollection.cs
+++ src/Persistence/Tests/Nabs.Tests.PersistenceTests/Fixtures/TestDbContextDataLoaderFixtureCollection.cs
@@ -1,7 +1,7 @@
 ﻿namespace Nabs.Tests.PersistenceTests.Fixtures;
 
 [CollectionDefinition("TestDbContextDataLoader")]
-public sealed class TestDbContextDataLoaderCollectionFixture : ICollectionFixture<DataLoaderFixture>
+public sealed class TestDbContextDataLoaderFixtureCollection : ICollectionFixture<DataLoaderFixture>
 {
     
 }
